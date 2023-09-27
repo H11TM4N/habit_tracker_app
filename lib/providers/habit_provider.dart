7e967034c8b877bd068c3b633119e868e02e4966
JER3MIAH <jeremiahbrownie@gmail.com>
2023-09-27 17:28:56 +0100

    update

--- lib/providers/habit_provider.dart
+++ lib/providers/habit_provider.dart
@@ -6,18 +6,48 @@ import '../widgets/progress_painter.dart';
 class HabitProvider extends ChangeNotifier {
   final List<String> items = ['1'];
   final double progress = 3;
+  bool toggle = false;
+
+  Widget customListTile(int index) {
+    return ListTile(
+      key: Key('$index'),
+      title: Text(items[index]),
+      leading: CustomPaint(
+        size: const Size(20, 20), // Size of the progress circle
+        painter: ProgressPainter(progress: progress),
+      ),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          GestureDetector(
+            onLongPress: () {
+              toggle = !toggle;
+              notifyListeners();
+            },
+            child: toggle ? const Icon(Icons.check) : const Icon(Icons.close),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          const Icon(Icons.close),
+          const SizedBox(
+            width: 20,
+          ),
+          const Icon(Icons.check),
+          const SizedBox(
+            width: 20,
+          ),
+          const Icon(Icons.close),
+        ],
+      ),
+    );
+  }
 
   List<Widget> customList() {
     return List.generate(
-        items.length,
-        (index) => ListTile(
-              key: Key('$index'),
-              title: Text(items[index]),
-              leading: CustomPaint(
-                size: const Size(20, 20), // Size of the progress circle
-                painter: ProgressPainter(progress: progress),
-              ),
-            ));
+      items.length,
+      (index) => customListTile(index),
+    );
   }
 
   void onReorder(int oldIndex, int newIndex) {
--- lib/widgets/habit_item.dart
+++ lib/widgets/habit_item.dart
@@ -5,11 +5,17 @@
 // class HabitListItem extends StatelessWidget {
 //   final String habitName;
 //   final double progress;
-//   const HabitListItem({super.key, required this.habitName, required this.progress});
+//   final int index;
+//   const HabitListItem(
+//       {super.key,
+//       required this.habitName,
+//       required this.progress,
+//       required this.index});
 
 //   @override
 //   Widget build(BuildContext context) {
 //     return ListTile(
+//       key: Key('$index'),
 //       title: Text(habitName),
 //       trailing: CustomPaint(
 //         size: const Size(20, 20), // Size of the progress circle
