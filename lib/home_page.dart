d29e3cc5316b2ea509ed1809f2c77f68d6359906
JER3MIAH <jeremiahbrownie@gmail.com>
2023-09-27 08:20:03 +0100

    reorderable list view

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
+    final List<int> items = List<int>.generate(30, (int index) => index);
+
+    void onReorder(int oldIndex, int newIndex) {
+      setState(() {
+        if (oldIndex < newIndex) {
+          newIndex -= 1;
+        }
+        final item = items.removeAt(oldIndex);
+        items.insert(newIndex, item);
+      });
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Habits'),
@@ -22,6 +39,17 @@ class HomePage extends StatelessWidget {
             icon: const Icon(Icons.more_vert),
           )
         ],
+      ),
+      body: ReorderableListView(
+        onReorder: onReorder,
+        children: List.generate(
+          items.length,
+          (index) => ListTile(
+            key: Key('$index'),
+            title: Text('Item ${items[index]}'),
+            trailing: const Icon(Icons.drag_handle_sharp),
+          ),
+        ),
       ),
     );
   }
