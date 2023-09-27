4504dfc20c109872cb8a50d61800723968e6447c
JER3MIAH <jeremiahbrownie@gmail.com>
2023-09-27 15:22:25 +0100

    creating a custom painter

---
+++ lib/widgets/progress_painter.dart
@@ -0,0 +1,35 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class ProgressPainter extends CustomPainter {
+  final double progress;
+
+  ProgressPainter({required this.progress});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint()
+      ..color = Colors.blue // Color of the progress circle
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2.0; // Adjust the thickness of the circle
+
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = min(size.width / 2, size.height / 2);
+
+    canvas.drawCircle(center, radius, paint);
+
+    final progressPaint = Paint()
+      ..color = Colors.blue // Color of the progress arc
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 4.0; // Adjust the thickness of the arc
+
+    final progressAngle = 2 * pi * progress;
+    final rect = Rect.fromCircle(center: center, radius: radius);
+    canvas.drawArc(rect, -pi / 2, progressAngle, false, progressPaint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
