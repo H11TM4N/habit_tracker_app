2153188b2d69a570e4d72e10ee592217a55c0773
JER3MIAH <jeremiahbrownie@gmail.com>
2023-09-28 08:41:19 +0100

    update

--- lib/widgets/habit_item.dart
+++ lib/widgets/habit_item.dart
@@ -1,30 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// import 'progress_painter.dart';
-
-// class HabitListItem extends StatelessWidget {
-//   final String habitName;
-//   final double progress;
-//   final int index;
-//   const HabitListItem(
-//       {super.key,
-//       required this.habitName,
-//       required this.progress,
-//       required this.index});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListTile(
-//       key: Key('$index'),
-//       title: Text(habitName),
-//       trailing: CustomPaint(
-//         size: const Size(20, 20), // Size of the progress circle
-//         painter: ProgressPainter(progress: progress),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
