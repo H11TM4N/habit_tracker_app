d87f52601bc4446551ce48364cdb12084e4fc227
JER3MIAH <jeremiahbrownie@gmail.com>
2023-09-28 10:19:05 +0100

    Select color

--- lib/models/habits.dart
+++
@@ -1,9 +0,0 @@
-class Habit {
-  final String name;
-  final String question;
-
-  Habit({
-    required this.name,
-    required this.question, required String description,
-  });
-}
--- lib/screens/yes_or_no_screen.dart
+++ lib/screens/yes_or_no_screen.dart
@@ -4,8 +4,53 @@ import 'package:provider/provider.dart';
 
 import '../providers/habit_provider.dart';
 
-class YesOrNoScreen extends StatelessWidget {
+class YesOrNoScreen extends StatefulWidget {
   const YesOrNoScreen({super.key});
+
+  @override
+  State<YesOrNoScreen> createState() => _YesOrNoScreenState();
+}
+
+class _YesOrNoScreenState extends State<YesOrNoScreen> {
+  Color? habitTextColor; // Store the selected text color for the habit
+
+  // Define a list of colors to choose from
+  final List<Color> colorOptions = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.orange,
+    Colors.purple,
+  ];
+
+  // Function to show a dialog with color options
+  Future<void> showColorSelectionDialog(BuildContext context) async {
+    final selectedColor = await showDialog<Color>(
+      context: context,
+      builder: (context) {
+        return SimpleDialog(
+          title: const Text('Select Text Color'),
+          children: colorOptions.map((color) {
+            return SimpleDialogOption(
+              onPressed: () {
+                Navigator.pop(context, color);
+              },
+              child: Container(
+                color: color,
+                height: 40,
+              ),
+            );
+          }).toList(),
+        );
+      },
+    );
+
+    if (selectedColor != null) {
+      setState(() {
+        habitTextColor = selectedColor;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,10 +73,39 @@ class YesOrNoScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          CustomTextField(
-            title: 'Name',
-            hintText: 'Exercise',
-            controller: nameController,
+          Row(
+            children: [
+              Expanded(
+                child: CustomTextField(
+                  title: 'Name',
+                  hintText: 'Exercise',
+                  controller: nameController,
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  showColorSelectionDialog(context);
+                },
+                child: SizedBox(
+                  width: 90,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListTile(
+                      tileColor: habitTextColor ?? Colors.black12,
+                      title: const Center(
+                        child: Text(
+                          'Color',
+                          style: TextStyle(
+                            color: Colors.white, // Set a default text color
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
           CustomTextField(
             title: 'Question',
