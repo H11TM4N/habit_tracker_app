458a56aa2064072f4357f0aac5c872b64679dc3c
JER3MIAH <jeremiahbrownie@gmail.com>
2023-09-28 08:43:37 +0100

    update

--- lib/widgets/habit_item.dart
+++ lib/widgets/habit_item.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import '../providers/habit_provider.dart';
 
 class HabitTile extends StatelessWidget {
@@ -18,29 +17,32 @@ class HabitTile extends StatelessWidget {
   Widget build(BuildContext context) {
     HabitProvider habitProvider = Provider.of<HabitProvider>(context);
 
-    return ListTile(
-      contentPadding: const EdgeInsets.all(9),
-      tileColor: Colors.black26,
-      title: Text(title),
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          GestureDetector(
-            onTap: onTap,
-            child: isDone
-                ? const Icon(
-                    Icons.check,
-                    color: Colors.amber,
-                  )
-                : const Icon(
-                    Icons.close,
-                    color: Colors.blue,
-                  ),
-          ),
-          const SizedBox(
-            width: 20,
-          ),
-        ],
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: ListTile(
+        contentPadding: const EdgeInsets.all(9),
+        tileColor: Colors.black26,
+        title: Text(title),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            GestureDetector(
+              onTap: onTap,
+              child: isDone
+                  ? const Icon(
+                      Icons.check,
+                      color: Colors.amber,
+                    )
+                  : const Icon(
+                      Icons.close,
+                      color: Colors.blue,
+                    ),
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+          ],
+        ),
       ),
     );
   }
